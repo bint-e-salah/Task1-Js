08355f2216c3632b4ea263b1dd6aa7d427d2c740
bint-e-salah <quratulainsalahuddin07@gmail.com>
2023-04-07 17:13:06 +0500

    FEAT; add file

---
+++ script/app.js
@@ -0,0 +1,6 @@
+var zakatPercentage= 0.025;
+var userInput= +prompt("Enter your input value");
+var result= zakatPercentage * userInput;
+console.log(result);
+var result = 25;
+alert("your zakat value is" + result);
